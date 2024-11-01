36f7554e8e3dad7486aff9efe93a4da5fee3a7a3
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-10-31 20:48:24 -0700

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function ContactusPage  ()  {
+  return (
+    <div>
+      <h1>Contact us</h1>
+      <h2>Email</h2>
+      <p>imranhassankhannzada@gmail.com</p>
+      <h3>facebook</h3>
+      <p>imranhassan/facebook.com</p>
+      <h4>twitter</h4>
+      <p>imranhassan/twitter.com</p>
+      <h5>instagram</h5>
+      <p>imranhassan/instagram.com</p>
+    </div>
+  )
+}
+
+export default ContactusPage
