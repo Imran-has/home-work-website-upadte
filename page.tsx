d28b1025e636341d1c7a3eb60b02d34081dde2b5
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-10-31 20:50:00 -0700

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,45 +1,18 @@
 import React from 'react'
-import cssStyles from "./Freelancingservics.module.css"
+import Links from"./Components/links"
+import Footer from "./Components/Footer"
 
-console.log(cssStyles,"CSS-Styles")
 
-function Freelancingservicespage ()  {
+
+export default function Homepage () {
   return (
-    <div className='free'>
-      <h1 className={cssStyles.free}><i><b>Our freelancing services</b></i></h1>
-      <h2>Freelancing services</h2>
-      <p>we are dedicated and experiene (5 years) [Graphics designing, content writing,SEO,Video editing,wordpress website developer,Digital marketing]
-        we are specialize in list key skills service, and we are passionate about helping clients achieve their gols through innovative
-        and effective solutions
-
-        <h1 >service we offer </h1><br/>
-        <ul>
-            <li>Graphics designing</li><br/>
-            <li> Content writing</li><br/>
-            <li>SEO</li><br/>
-            <li>Video editing</li><br/>
-            <li>wordpress website developer</li><br/>
-            <li>Digital marketing</li><br/>
-        </ul>
-      </p>
+    <div>
+      
+      
+      
     </div>
-    
   )
 }
-
-export default  Freelancingservicespage
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
